bef1ba5f159ffbfc1658ae6a11b8ab8af98b557b
Braden McDorman <bmcdorman@gmail.com>
2015-10-30 15:33:39 -0700

    done

--- index.ts
+++ index.ts
@@ -11,18 +11,15 @@ declare class process
   static env: Env;
 }
 
+///////
+
 var express = require("express");
 var app = express();
 
-app.get("/", (req, res) => {
-  res.sendfile('index.html')
-});
+console.log(__dirname + '/public');
+app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/'));
 
-app.use(app.router);
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/build'));
-
-var port = process.env.PORT || 5000;
-app.listen(port, () => {
-   console.log("Listening on " + port);
+app.listen(8888, () => {
+   console.log("Listening on " + 8888);
 });
