a02d2a334dfebf673846b7357c276fe6b0ba8b6f
Braden McDorman <bmcdorman@gmail.com>
2015-10-30 15:24:52 -0700

    scaffolding complete

--- index.ts
+++ index.ts
@@ -1,11 +1,25 @@
+declare var __dirname: string;
+declare function require(s: string): any;
+
+declare class Env
+{
+  PORT: number;
+}
+
+declare class process
+{
+  static env: Env;
+}
+
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.sendfile('index.html')
 });
 
 app.use(app.router);
+app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/build'));
 
 var port = process.env.PORT || 5000;
